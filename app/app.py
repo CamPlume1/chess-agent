e20a663086b38901eba72275c10ba2d85919a82e
Adam Ma <adam.zym@gmail.com>
2025-04-02 16:33:22 -0400

    ...

--- app/app.py
+++ app/app.py
@@ -5,15 +5,13 @@ from app.strategies.mcts_strategy import MCTSStrategy
 from app.strategies.a_b_pruning_strategy import ABPruningStrategy
 from app.strategies.evaluators.neural_network_evaluator import NeuralNetworkEvaluator
 from app.strategies.evaluators.standard_evaluator import StandardEvaluator
-from app.strategies.abstrategy import Strategy
 from app.view.gui_view import ChessGui
 
 chess_board = chess.Board()
 evaluator1 = NeuralNetworkEvaluator()
-evaluator2 = StandardEvaluator()
 
 agent1 = MCTSStrategy(board=chess_board, evaluator=evaluator1, side=chess.WHITE)
-agent2 = ABPruningStrategy(board=chess_board, evaluator=evaluator2, side=chess.BLACK)
+agent2 = ABPruningStrategy(board=chess_board, evaluator=evaluator1, side=chess.BLACK)
 view = ChessGui(chess_board)
 controller = GameController(agent1, agent2, chess_board, view)
 print(controller.play_game())

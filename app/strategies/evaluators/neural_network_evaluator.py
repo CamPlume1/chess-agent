99ad9870a3a3553ed59a062b8907e4780f388f27
Adam Ma <adam.zym@gmail.com>
2025-04-08 12:33:16 -0400

    New NN

--- app/strategies/evaluators/neural_network_evaluator.py
+++ app/strategies/evaluators/neural_network_evaluator.py
@@ -4,4 +4,9 @@ from models.neural_network.neural_network import NeuralNetworkModel
 
 class NeuralNetworkEvaluator(PositionEvaluator):
     def evaluate(self, board: chess.Board) -> float:
+        if board.is_game_over():
+            print("End State Found")
+            print("State Evaluation: ", super()._game_over_evaluation(board))
+            return super()._game_over_evaluation(board)
+        
         return NeuralNetworkModel.evaluate_fen(board.fen(), "models/neural_network/neural_network.pth")

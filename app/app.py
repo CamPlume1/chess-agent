4d85ba4f59454831aa354ac71c9d8f5d5a686ca7
Adam Ma <adam.zym@gmail.com>
2025-04-17 14:10:10 -0400

    MCTS + FF vs Stockfish

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ view = ChessGui(initial_board)
 
 agent = MCTSStrategy(
     board=None,
-    evaluator=ConvolutionalNetworkEvaluator(),
+    evaluator=NeuralNetworkEvaluator(),
     side=None,
 )
 
@@ -45,7 +45,7 @@ centipawn_benchmark = StockfishStrategy(
 try:
     evaluator = ChessAgentEvaluator(
         agent=agent,
-        agent_name="MCTS + CNN",
+        agent_name="MCTS + FF",
         benchmark=stockfish,
         benchmark_name="Stockfish",
         benchmark_elo=1320,

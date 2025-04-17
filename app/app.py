91907b22554853632d889aa3140de92c234e0dd7
Adam Ma <adam.zym@gmail.com>
2025-04-17 12:11:16 -0400

    MCTS + CNN vs Random

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ view = ChessGui(initial_board)
 
 agent = MCTSStrategy(
     board=None,
-    evaluator=NeuralNetworkEvaluator(),
+    evaluator=ConvolutionalNetworkEvaluator(),
     side=None,
 )
 
@@ -45,7 +45,7 @@ centipawn_benchmark = StockfishStrategy(
 try:
     evaluator = ChessAgentEvaluator(
         agent=agent,
-        agent_name="MCTS + FF",
+        agent_name="MCTS + CNN",
         benchmark=random,
         benchmark_name="Random",
         benchmark_elo=500,

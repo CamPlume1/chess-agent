e40830e46a0dff86c6e37160473ce4e134fce042
Adam Ma <adam.zym@gmail.com>
2025-04-18 16:34:57 -0400

    Final AB Pruning vs Stockfish

--- app/strategies/evaluators/standard_evaluator.py
+++ app/strategies/evaluators/standard_evaluator.py
@@ -1,7 +1,6 @@
 from .abstract_evaluator import PositionEvaluator
 import chess
 
-
 class StandardEvaluator(PositionEvaluator):
     def evaluate(self, board: chess.Board) -> float:
         """
@@ -20,8 +19,6 @@ class StandardEvaluator(PositionEvaluator):
         score += self._king_safety_rewards(board)
         score += self._piece_positioning_reward(board)
         return score
-    
-
 
     def _material_balance(self, board: chess.Board):
         # Piece values (in centipawns) -> Very high king value to discincentivize losing King.
@@ -50,8 +47,6 @@ class StandardEvaluator(PositionEvaluator):
                 
                 material_balance += value
         return material_balance
-    
-
 
     def _pawn_structure_rewards(self, board: chess.Board) -> float:
         reward = 0
@@ -94,20 +89,20 @@ class StandardEvaluator(PositionEvaluator):
 
         # White king safety:
         if board.has_kingside_castling_rights(chess.WHITE) or board.has_queenside_castling_rights(chess.WHITE):
-            score += 15
+            score += 30
         else:
             white_king_square = board.king(chess.WHITE)
             # Penalize if the white king is in a central square - Not a safe space traditionally
-            if white_king_square in [chess.E1, chess.D1, chess.E2, chess.D2, chess.C2, chess.F2]:
-                score -= 35
+            if white_king_square in [chess.E1, chess.D1]:
+                score -= 50
 
         # Black king safety:
         if board.has_kingside_castling_rights(chess.BLACK) or board.has_queenside_castling_rights(chess.BLACK):
-            score -= 15
+            score -= 30
         else:
             black_king_square = board.king(chess.BLACK)
-            if black_king_square in [chess.E8, chess.D8, chess.E7, chess.D7, chess.F7, chess.C7]:
-                score += 35
+            if black_king_square in [chess.E8, chess.D8]:
+                score += 50
 
         return score
 
@@ -127,45 +122,14 @@ class StandardEvaluator(PositionEvaluator):
             [-50, -40, -30, -30, -30, -30, -40, -50]
         ]
 
-        pawn_table = [
-            [ 0,   0,   0,   0,   0,   0,   0,   0],
-            [50,  50,  50,  50,  50,  50,  50,  50],
-            [10,  10,  20,  30,  30,  20,  10,  10],
-            [ 5,   5,  10,  25,  25,  10,   5,   5],
-            [ 0,   0,   0,  20,  20,   0,   0,   0],
-            [ 5,  -5, -10,   0,   0, -10,  -5,   5],
-            [ 5,  10,  10, -20, -20,  10,  10,   5],
-            [ 0,   0,   0,   0,   0,   0,   0,   0]
-        ]
-
-        bishop_table = [
-            [-20, -10, -10, -10, -10, -10, -10, -20],
-            [-10,   5,   0,   0,   0,   0,   5, -10],
-            [-10,  10,  10,  10,  10,  10,  10, -10],
-            [-10,   0,  10,  10,  10,  10,   0, -10],
-            [-10,   5,   5,  10,  10,   5,   5, -10],
-            [-10,   0,   5,  10,  10,   5,   0, -10],
-            [-10,   0,   0,   0,   0,   0,   0, -10],
-            [-20, -10, -10, -10, -10, -10, -10, -20]
-        ]
-
         for square, piece in board.piece_map().items():
-            rank = chess.square_rank(square)
-            file = chess.square_file(square)
-            table = None
             if piece.piece_type == chess.KNIGHT:
-                table = knight_table
-            elif piece.piece_type == chess.PAWN:
-                table = pawn_table
-            elif piece.piece_type == chess.BISHOP:
-                table = bishop_table
-            else:
-                continue
-
-            if piece.color == chess.WHITE:
-                score += table[rank][file]
-            else:
-                score -= table[7 - rank][file]
+                rank = chess.square_rank(square)
+                file = chess.square_file(square)
+                if piece.color == chess.WHITE:
+                    score += knight_table[rank][file]
+                else:
+                    score -= knight_table[7 - rank][file]
 
         return score
 
